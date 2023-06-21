daff741082b6a657cb94decf97fdbea8fd96cc03
mikevhe18 <viriyananda.michael@gmail.com>
2023-06-21 16:35:13 +0700

    [ssi_partner_identification] 14.0.1.0.0

---
+++ ssi_partner_identification/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Partner Identification",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "depends": [
+        "ssi_partner",
+        "partner_identification",
+    ],
+    "data": [
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "views/res_partner_id_category_view.xml",
+        "views/res_partner_id_number_view.xml",
+    ],
+    "demo": [],
+}
