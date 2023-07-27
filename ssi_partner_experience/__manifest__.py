98532f47d76d5a9481d2b65abc8a679cca5d889e
mikevhe18 <viriyananda.michael@gmail.com>
2023-07-27 16:02:22 +0700

    [ssi_partner_experience] Change license to AGPL3

--- ssi_partner_experience/__manifest__.py
+++ ssi_partner_experience/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2023 OpenSynergy Indonesia
 # Copyright 2023 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Partner Experience",
     "version": "14.0.1.0.0",
     "website": "https://simetri-sinergi.id",
-    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
-    "license": "LGPL-3",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
     "installable": True,
     "application": True,
     "depends": [
