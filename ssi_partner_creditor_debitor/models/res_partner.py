3df124a0d3dad357fb193e9f80aea17f409d926b
Andhitia Rama <andhitia.r@gmail.com>
2024-02-15 04:22:11 +0700

    [ADD] ssi_partner_creditor_debitor

--- ssi_partner_creditor_debitor/models/res_partner.py
+++ ssi_partner_creditor_debitor/models/res_partner.py
@@ -3,12 +3,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
+    _name = "res.partner"
     _inherit = "res.partner"
 
+    primary_creditor_id = fields.Many2one(
+        string="Primary Creditor",
+        comodel_name="res.partner",
+        compute="_compute_primary_creditor_id",
+        store=True,
+    )
     creditor_ids = fields.One2many(
         string="Creditors",
         comodel_name="partner_creditor_debtor",
@@ -19,3 +26,15 @@ class ResPartner(models.Model):
         comodel_name="partner_creditor_debtor",
         inverse_name="creditor_id",
     )
+
+    @api.depends(
+        "creditor_ids",
+        "creditor_ids.sequence",
+        "creditor_ids.creditor_id",
+    )
+    def _compute_primary_creditor_id(self):
+        for record in self:
+            result = False
+            if record.creditor_ids:
+                result = record.creditor_ids[0].creditor_id
+            record.primary_creditor_id = result
