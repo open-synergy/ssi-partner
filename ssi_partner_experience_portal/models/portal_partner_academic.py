ce5c1e439fcfea1af36ef36d927f5564eca7c69d
Miftahussalam <miftah_salam@ymail.com>
2024-02-14 14:57:03 +0700

    [ADD] ssi_partner_experience

---
+++ ssi_partner_experience_portal/models/portal_partner_academic.py
@@ -0,0 +1,16 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class PortalPartnerAcademic(models.Model):
+    _name = "portal_partner_academic"
+    _inherit = [
+        'partner.academic'
+    ]
+    _description = "Portal Partner Academic"
+    _table = "partner_academic"
+    _order = "date_start"
---
+++ ssi_partner_experience_portal/models/portal_partner_experience.py
@@ -0,0 +1,16 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class PortalPartnerExperience(models.Model):
+    _name = "portal_partner_experience"
+    _inherit = [
+        'partner.experience'
+    ]
+    _description = "Portal Partner Experience"
+    _table = "partner_experience"
+    _order = "date_start"
