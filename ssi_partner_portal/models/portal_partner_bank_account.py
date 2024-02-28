27a993bc0a87eb76c0d644ec2299d31ce601a11d
mikevhe18 <viriyananda.michael@gmail.com>
2024-02-28 16:09:37 +0700

    [FIX] pre-commit

--- ssi_partner_portal/models/portal_partner_bank_account.py
+++ ssi_partner_portal/models/portal_partner_bank_account.py
@@ -2,15 +2,12 @@
 # Copyright 2024 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
+from odoo import models
 
 
 class PortalPartnerBankAccount(models.Model):
     _name = "portal_partner_bank_account"
-    _inherit = [
-        'res.partner.bank'
-    ]
+    _inherit = ["res.partner.bank"]
     _description = "Portal Partner Bank Account"
     _table = "res_partner_bank"
     _order = "acc_number"
