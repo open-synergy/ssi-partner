9271eec43a004c22d8e16f35c2464768b71cfa16
Miftahussalam <miftah_salam@ymail.com>
2024-06-11 17:26:50 +0700

    [UPD] ssi_partner_public_offering

--- ssi_partner_public_offering/models/company_public_offering_type.py
+++ ssi_partner_public_offering/models/company_public_offering_type.py
@@ -2,10 +2,15 @@
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import models, fields
 
 
 class CompanyPublicOfferingType(models.Model):
     _name = "company_public_offering_type"
     _inherit = ["mixin.master_data"]
     _description = "Company Public Offering Type"
+
+    p2pk_go_public = fields.Boolean(
+        string="P2PK Go Public",
+        required=False
+    )
