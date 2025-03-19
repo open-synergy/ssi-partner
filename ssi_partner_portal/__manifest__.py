a9e835c9e5b44a07694c194d5dafc255903d6106
mikevhe18 <viriyananda.michael@gmail.com>
2025-03-19 13:38:56 +0700

    [CI] C8101

--- ssi_partner_portal/__manifest__.py
+++ ssi_partner_portal/__manifest__.py
@@ -1,6 +1,7 @@
 # Copyright 2023 OpenSynergy Indonesia
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=C8101
 {
     "name": "Partner Portal",
     "version": "14.0.1.2.0",
