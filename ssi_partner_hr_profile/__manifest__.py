434b30a8c9022888e105678acc7da1ed3c9544bb
mikevhe18 <viriyananda.michael@gmail.com>
2025-03-19 13:17:55 +0700

    [ssi_partner_hr_profile] pre-commit fix

--- ssi_partner_hr_profile/__manifest__.py
+++ ssi_partner_hr_profile/__manifest__.py
@@ -1,6 +1,7 @@
 # Copyright 2025 OpenSynergy Indonesia
 # Copyright 2025 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=C8101
 {
     "name": "Partner App & HR My Profile Extension",
     "version": "14.0.1.0.0",
@@ -17,5 +18,5 @@
         "views/res_users_views.xml",
     ],
     "demo": [],
-    "auto_install": True
+    "auto_install": True,
 }
