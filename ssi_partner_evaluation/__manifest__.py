d9f31a3378030fa1836fab03f5e6d2a97be1a73a
mikevhe18 <viriyananda.michael@gmail.com>
2025-03-19 13:37:56 +0700

    [CI] C8101

--- ssi_partner_evaluation/__manifest__.py
+++ ssi_partner_evaluation/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2024 OpenSynergy Indonesia
 # Copyright 2024 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
+# pylint: disable=C8101
 {
     "name": "Partner Evaluation",
     "version": "14.0.1.2.0",
