307e9b4ae0b9e436eb829139f3a442db9b271db6
Andhitia Rama <andhitia.r@gmail.com>
2025-08-08 04:15:39 +0700

    [UPD] ssi_partner_evaluation
    * Menambahkan informasi previous result
    * Menambahkan informasi evaluation tag

--- ssi_partner_evaluation/models/res_partner.py
+++ ssi_partner_evaluation/models/res_partner.py
@@ -2,7 +2,7 @@
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
@@ -22,6 +22,34 @@ class ResPartner(models.Model):
         inverse_name="partner_id",
         readonly=True,
     )
+    evaluation_tag_ids = fields.Many2many(
+        string="Evaluation Tags",
+        comodel_name="res.partner.category",
+        compute="_compute_evaluation_tag_ids",
+        store=True,
+        compute_sudo=True,
+        relation="rel_partner_2_evaluation_tag",
+        column1="res_partner_id",
+        column2="tag_id",
+    )
+
+    @api.depends(
+        "partner_evaluation_result_ids",
+        "partner_evaluation_result_ids.latest_evaluation_id",
+    )
+    def _compute_evaluation_tag_ids(self):
+        Evaluation = self.env["res.partner.evaluation_result"]
+        for record in self:
+            result = []
+            criteria = [
+                ("partner_id", "=", record.id),
+            ]
+            evaluations = Evaluation.search(criteria)
+            if len(evaluations) > 0:
+                result = evaluations.mapped(
+                    "latest_evaluation_id.final_result_id.tag_id"
+                )
+            record.evaluation_tag_ids = result
 
     def _get_partner_evaluation_result(self, evaluation_type):
         self.ensure_one()
--- ssi_partner_evaluation/models/res_partner_evaluation_result.py
+++ ssi_partner_evaluation/models/res_partner_evaluation_result.py
@@ -38,6 +38,28 @@ class ResPartnerEvaluationResult(models.Model):
         store=True,
     )
 
+    previous_evaluation_id = fields.Many2one(
+        string="Previous Evaluation",
+        comodel_name="partner_evaluation",
+        compute="_compute_latest_evaluation_id",
+        store=True,
+        compute_sudo=True,
+    )
+    previous_result_id = fields.Many2one(
+        string="Previous Result",
+        related="previous_evaluation_id.final_result_id",
+        store=True,
+    )
+    previous_date = fields.Date(
+        related="previous_evaluation_id.date",
+        store=True,
+    )
+    diff_evaluation = fields.Boolean(
+        string="Latest Diff Than Previous",
+        compute="_compute_latest_evaluation_id",
+        store=True,
+    )
+
     @api.depends(
         "partner_id",
         "partner_id.partner_evaluation_ids",
@@ -47,10 +69,19 @@ class ResPartnerEvaluationResult(models.Model):
     )
     def _compute_latest_evaluation_id(self):
         for record in self:
-            result = False
+            latest = previous = diff = False
             evaluations = record.partner_id.partner_evaluation_ids.filtered(
                 lambda r: r.state == "done" and r.type_id.id == record.type_id.id
             )
             if evaluations:
-                result = evaluations[0]
-            record.latest_evaluation_id = result
+                latest = evaluations[0]
+
+            if len(evaluations) >= 2:
+                previous = evaluations[1]
+
+            if latest != previous and previous:
+                diff = True
+
+            record.latest_evaluation_id = latest
+            record.previous_evaluation_id = previous
+            record.diff_evaluation = diff
