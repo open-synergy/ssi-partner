03cbc47bb64edd19560f3d03434bca3345fbdacf
Andhitia Rama <andhitia.r@gmail.com>
2026-05-23 08:51:16 +0700

    [UPD] ssi_partner_experience
    * Add unit tests

---
+++ ssi_partner_experience/tests/test_partner_experience.py
@@ -0,0 +1,13 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo_yaml_test import YamlTransactionCase
+
+from odoo.tests import tagged
+
+
+@tagged("post_install", "-at_install")
+class TestPartnerExperience(YamlTransactionCase):
+    def test_partner_experience(self):
+        self.run_yaml_scenario("test_data_partner_experience.yaml")
