c45224f2aa868afdecac1355d662d16c77a67bc6
Andhitia Rama <andhitia.r@gmail.com>
2026-05-23 08:51:33 +0700

    [UPD] ssi_partner_public_offering
    * Add unit tests

--- ssi_partner_public_offering/tests/__init__.py
+++ ssi_partner_public_offering/tests/__init__.py
@@ -2,6 +2,4 @@
 # Copyright 2024 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import models
-
-from . import tests  # noqa: F401
+from . import test_public_offering  # noqa: F401
