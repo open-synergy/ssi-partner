c4930ba39a1a7b7c44ab417a36d8573ac6952dfd
Andhitia Rama <andhitia.r@gmail.com>
2026-07-24 22:24:48 +0700

    [ADD] ssi_partner_data_privacy_consent
    Modul fondasi master data untuk consent privasi data partner:
    
    * partner_consent_purpose - daftar tujuan pemrosesan data.
    * partner_consent_notice - versi dokumen privacy notice beserta teks
      yang disetujui, tanggal efektif, dan tujuan yang dicakup.
    
    Menu Data Privacy di bawah Contacts -> Configuration, group configurator
    sendiri per master data, dan unit test skenario odoo-yaml-test.

---
+++ ssi_partner_data_privacy_consent/models/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    partner_consent_purpose,
+    partner_consent_notice,
+)
